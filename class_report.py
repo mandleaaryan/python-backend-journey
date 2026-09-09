1b95ab9bff46ce95cd40dacbe4000fc5f246a09e
mandleaaryan <mandleaaryan@gmail.com>
2026-09-09 21:51:50 +0530

    Day 13: Complete class report system

---
+++ class_report.py
@@ -0,0 +1,43 @@
+def count_passed(students):
+    count = 0
+
+    for student in students:
+        if student["average"] >= 85:
+            count += 1
+
+    return count
+
+
+def find_top_student(students):
+    top_student = students[0]
+
+    for student in students:
+        if student["average"] > top_student["average"]:
+            top_student = student
+
+    return top_student
+
+
+def calculate_class_average(students):
+    total = 0
+
+    for student in students:
+        total += student["average"]
+
+    return total / len(students)
+
+
+students = [
+    {"name": "Aaryan", "average": 90},
+    {"name": "Tokyo", "average": 83},
+    {"name": "Japan", "average": 91},
+    {"name": "Delhi", "average": 78}
+]
+
+print("Passed:", count_passed(students))
+
+top = find_top_student(students)
+
+print("Top Student:", top["name"])
+
+print("Class Average:", calculate_class_average(students))
